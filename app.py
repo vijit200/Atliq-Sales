d83a53726a335b1dc2c69c729473f6212770d2fe
vijit kumar <vijitkumar699@gmail.com>
2022-10-06 02:23:29 +0530

    op

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import flask
 from flask import render_template,Flask
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-@app.route("/",methods=['GET'])
+@app.route("/atliq",methods=['GET'])
 @cross_origin()
 def index():
-    return(render_template('index.html'))
+    return('<iframe title="Sales insight" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=4e1312ed-87fa-43bb-ba27-159f86f4fa61&autoAuth=true&ctid=0df4f8da-952d-4c0a-80fa-408c57b08ef6" frameborder="0" allowFullScreen="true"></iframe>')
 
 if __name__ == '__main__':
     app.run(debug=True)
