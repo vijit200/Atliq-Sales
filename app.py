ada2421fa8f1c488162e5f50b44aabf8aea480f6
vijit kumar <vijitkumar699@gmail.com>
2022-10-06 02:18:51 +0530

    up

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import flask
 from flask import render_template,Flask
-
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-@app.route('/',methods=['GET'])
+@app.route("/",methods=['GET'])
+@cross_origin()
 def index():
     return(render_template('index.html'))
 
