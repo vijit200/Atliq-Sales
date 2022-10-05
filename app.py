22f6188097497baae55cad7e639886c3d3b5369a
vijit kumar <vijitkumar699@gmail.com>
2022-10-06 02:14:54 +0530

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import flask
 from flask import render_template,Flask
 
 app = Flask(__name__)
-@app.route('/',methods=['POST','GET'])
+@app.route('/',methods=['GET'])
 def index():
     return(render_template('index.html'))
 
